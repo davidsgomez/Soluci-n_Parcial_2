d554517aa949648f8cb86d936fef7df11f5e80da
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-04-18 20:59:32 -0600

    Update Solución_Parcial_2.cs

--- Solución_Parcial_2.cs
+++ Solución_Parcial_2.cs
@@ -1,13 +1,13 @@
 
-int datoa = 16;
-String datob = "25g";
-int resultado = datol + Convert. ToInt32( dato2);
+int dato1 = 16;
+String dato2 = "25g";
+int resultado = dato1 + Convert. ToInt32( dato2);
 
-int datoa = 16;
-String datob = "25g";
+int dato1 = 16;
+String dato2 = "25g";
 
 try {
-    int resultado = datoa + Convert.ToInt32(datob);
+    int resultado = dato1 + Convert.ToInt32(dato2);
     Console.WriteLine("El resultado es: " + resultado);
 } catch (FormatException e) {
     Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
